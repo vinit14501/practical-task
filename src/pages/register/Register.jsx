c2129f093114536463bac582838031e37bfc6495
vinit14501 <57012278+vinit14501@users.noreply.github.com>
2023-05-02 22:29:29 +0530

    first deploy

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import FormInput from '../../components/formInput/FormInput';
 import { Link, useNavigate } from 'react-router-dom';
 import { updateProfile, createUserWithEmailAndPassword } from 'firebase/auth';
